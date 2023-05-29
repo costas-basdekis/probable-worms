b64b3e054c68783b90476fdf3a9840d911cd295c
Costas Basdekis <costas.basdekis@gmail.com>
2023-05-29 10:27:00 +0100

    Add favicon and manifest

---
+++ src/Credits.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export class Credits extends Component {
+  render() {
+    return (
+      <>
+        Worm favicon by
+        <a href={"https://www.flaticon.com/free-icons/worm"} title={"worm icons"}>Worm icons created by Freepik - Flaticon</a>
+      </>
+    );
+  }
+}
